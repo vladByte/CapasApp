f3a4147bb61b62d13c18e3b3dce2816972cb5c9c
Vlad <vjerez@bnc.com.ve>
2023-10-17 15:00:35 -0400

    Estabilizando Archivos

---
+++ CapasApp2/Controllers/TipoMedicamentoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CapasApp2.Controllers
+{
+    public class TipoMedicamentoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
