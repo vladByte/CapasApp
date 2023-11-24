ee682b12b7e1d66f23d4e7dd8fc82703c44abd52
vladByte <vlad.jerez@gmail.com>
2023-11-23 21:44:25 -0400

    Progreso cnx BD
    Se configuro el appsettings para la conexion de BD

--- CapasApp2/Controllers/MedicamentosController.cs
+++ CapasApp2/Controllers/MedicamentosController.cs
@@ -1,18 +1,26 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.IO;
+using System;
+using System.Data;
+using System.ComponentModel.Design.Serialization;
+using System.Collections.Immutable;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CapasApp2.Controllers
 {
     public class MedicamentosController : Controller
     {
-        public IActionResult Index()
-        {
-            return View();
-        }
+        // public IActionResult Index()
+        // {
+        //     return View();
+        // }
 
-        public IActionResult MedicamentosList()
-
+        public string Cadena()
         {
-            return View("Medicamentos/MedicamentosList");
+            IConfigurationBuilder builder = new ConfigurationBuilder();
+            builder.AddJsonFile(Path.Combine(Directory.GetCurrentDirectory(),"appsettings.json"));
+            var root = builder.Build();
+            var db = root.GetConnectionString("cnx");
+            return db; 
         }
      
     }
